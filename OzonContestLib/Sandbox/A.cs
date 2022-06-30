aae2e05499ac26b69157fbf45cc18bd5469fbc51
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-06-30 16:59:28 +0300

    Added sandbox A handler

---
+++ OzonContestLib/Sandbox/A.cs
@@ -0,0 +1,27 @@
+﻿using OzonContest.Helpers;
+
+namespace OzonContestLib.Sandbox
+{
+    public class A : IssueHandlerBase
+    {
+        public A()
+        {
+        }
+
+        public A(IReader reader, IWriter writer) : base(reader, writer)
+        {
+        }
+
+        public override int Number { get; } = 1;
+
+        public override void Run()
+        {
+            int count = ReadInt();
+            for (int i = 0; i < count; i++)
+            {
+                var strs = Read2Int();
+                Write(strs.Item1 + strs.Item2);
+            }
+        }
+    }
+}
---
+++ OzonContestTests/SandBox.cs
@@ -0,0 +1,18 @@
+using OzonContest.Helpers;
+using OzonContestLib.Sandbox;
+
+namespace OzonContestTests
+{
+    [TestClass]
+    public class SandBox : BaseTest
+    {
+        public SandBox()
+        {
+            DatasetName = nameof(SandBox);
+        }
+
+        [TestMethod]
+        public void A() 
+            => ExecuteTest((IReader reader, IWriter validator) => new A(reader, validator));
+    }
+}
