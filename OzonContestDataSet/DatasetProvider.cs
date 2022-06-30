db32e4f25efcf35f923b397466afeaf6bf137f9c
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-06-30 16:56:45 +0300

    Create DatasetProvider.cs

---
+++ OzonContestDataSet/DatasetProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Compression;
+
+namespace OzonContestDataSet
+{
+    public class DatasetProvider
+    {
+        private const string _dirWithFiles = "files";
+
+        public static IEnumerable<(string question, string answer)> GetFilesNames(string datasetName, string issueName)
+        {
+            EnsureFilesCreated(datasetName, issueName);
+            string targetDir = Path.Combine(_dirWithFiles, datasetName, issueName);
+            string[] fileNames = Directory.GetFiles(targetDir);
+            for (int i = 0; i < fileNames.Length; i += 2)
+                yield return (fileNames[i], fileNames[i + 1]);
+        }
+
+        private static void EnsureFilesCreated(string datasetName, string issueName)
+        {
+            string targetDir = Path.Combine(_dirWithFiles, datasetName, issueName);
+            if (Directory.Exists(targetDir) && Directory.GetFiles(targetDir).Length != 0)
+                return;
+            Directory.CreateDirectory(targetDir);
+            using FileStream sourceFs = new(Path.Combine(datasetName, issueName + ".zip"), FileMode.Open, FileAccess.Read);
+            using ZipArchive zip = new(sourceFs);
+            foreach (var entry in zip.Entries.Where(x=>x.Length > 0))
+                entry.ExtractToFile(Path.Combine(targetDir, entry.Name), true);
+        }
+    }
+}
