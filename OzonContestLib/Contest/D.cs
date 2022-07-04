b19f48c56aa6e2cae122a380e980e3bda1bafa80
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-07-04 11:23:37 +0300

    Added contest D handler

--- OzonContestLib/Contest/D.cs
+++ OzonContestLib/Contest/D.cs
@@ -14,9 +14,66 @@ namespace OzonContestLib.Contest
 
         public override int Number { get; }
 
+        /// <summary>
+        /// todo подумать над тестами c несколькими вариантами ответов, решение правильное
+        /// </summary>
         public override void Run()
         {
-            throw new NotImplementedException();
+            //лютейший говнокод. Торопился
+            char[] glasnie = new[] { 'e', 'u', 'i', 'o', 'a', 'y', 'E', 'U', 'I', 'O', 'A', 'Y' };
+            int count = ReadInt();
+            for (int i = 0; i < count; i++)
+            {
+                string pass = ReadLine();
+
+                bool olnyUpper = pass.ToUpperInvariant() == pass, olnyLower = pass.ToLowerInvariant() == pass;
+                bool anyGlasnie = false, anyNotGlasnie = false, anyDigit = false;
+                foreach (char c in pass)
+                {
+                    anyGlasnie |= glasnie.Contains(c);
+                    anyNotGlasnie |= !glasnie.Contains(c) && !char.IsDigit(c);
+                    anyDigit |= char.IsDigit(c);
+                }
+                if (olnyLower)
+                {
+                    if (!anyGlasnie)
+                    {
+                        anyGlasnie = true;
+                        pass = pass + "E";
+                    }
+                    else
+                    {
+                        anyNotGlasnie = true;
+                        pass = pass + "D";
+                    }
+                }
+                if (olnyUpper)
+                {
+                    if (!anyGlasnie)
+                    {
+                        anyGlasnie = true;
+                        pass = pass + "e";
+                    }
+                    else
+                    {
+                        anyNotGlasnie = true;
+                        pass = pass + "d";
+                    }
+                }
+                if (!anyGlasnie)
+                {
+                    anyGlasnie = true;
+                    pass = pass + "e";
+                }
+                if (!anyNotGlasnie)
+                {
+                    anyNotGlasnie = true;
+                    pass = pass + "d";
+                }
+                if (!anyDigit)
+                    pass = pass + "1";
+                Write(pass);
+            }
         }
     }
 }
