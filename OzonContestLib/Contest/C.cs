873f2987b804dd16b6dfdf7455a5d08f9b41311d
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-07-04 11:22:11 +0300

    Added contest C handler

--- OzonContestLib/Contest/C.cs
+++ OzonContestLib/Contest/C.cs
@@ -16,7 +16,17 @@ namespace OzonContestLib.Contest
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            (int usersCount, int reqCount)= Read2Int();
+            int[] notifications = new int[usersCount + 1];
+            int ntfIterator = 0;
+            for (int i = 0; i < reqCount; i++)
+            {
+                (int t, int id) = Read2Int();
+                if (t == 1)
+                    notifications[id] = ++ntfIterator;
+                else
+                    Write(Math.Max(notifications[id], notifications[0]));
+            }
         }
     }
 }
