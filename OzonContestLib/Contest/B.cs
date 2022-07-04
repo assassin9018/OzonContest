215a172c352214705b8abf90c475d47a51f06859
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-07-04 11:21:51 +0300

    Added contest B handler

--- OzonContestLib/Contest/B.cs
+++ OzonContestLib/Contest/B.cs
@@ -14,9 +14,32 @@ namespace OzonContestLib.Contest
 
         public override int Number { get; }
 
+        //todo проверить тест, решение правильное
         public override void Run()
         {
-            throw new NotImplementedException();
+            int count = ReadInt();
+            for (int i = 0; i < count; i++)
+            {
+                _ = ReadInt();
+                var devs = ReadArray();
+                for (int j = 0; j < devs.Length; j++)
+                {
+                    var current = devs[j];
+                    if (current == int.MinValue)
+                        continue;
+                    int otherDelta = int.MaxValue, otherIndex = -1;
+                    for (int k = j + 1; k < devs.Length; k++)
+                        if (Math.Abs(current - devs[k]) < otherDelta)
+                        {
+                            otherDelta = Math.Abs(current - devs[k]);
+                            otherIndex = k;
+                        }
+                    devs[otherIndex] = int.MinValue;
+                    Write($"{j + 1} {otherIndex + 1}");
+                }
+                Write();
+            }
         }
+
     }
 }
