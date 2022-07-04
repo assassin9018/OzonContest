a7917d4e9bc51132ec8fdc4922b707861933650a
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-07-04 11:21:39 +0300

    Added contest A handler

--- OzonContestLib/Contest/A.cs
+++ OzonContestLib/Contest/A.cs
@@ -16,7 +16,7 @@ namespace OzonContestLib.Contest
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            Write("I am sure that I will fill out the form by 11:00 am on July 4, 2022.");
         }
     }
 }
